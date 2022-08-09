b6b014f0a1e505c2d7b71a11c9d060d0ebd6dee8
小山由人 <koyama@reytech.co.jp>
2022-08-09 22:48:32 +0900

    initial setup

---
+++ src/main/java/com/raisetech/bookcase/BookcaseApplication.java
@@ -0,0 +1,13 @@
+package com.raisetech.bookcase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookcaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookcaseApplication.class, args);
+	}
+
+}
